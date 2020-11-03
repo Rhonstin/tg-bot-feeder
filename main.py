5c36dd87477aa67ccbbb4ddf2e4e3f5232b66270
Rhonstin <rhonstin@gmail.com>
2020-11-03 23:51:15 +0200

    add condition to privat chat

--- main.py
+++ main.py
@@ -18,27 +18,24 @@ client.parse_mode = "html"
 db_connect = sqlite3.connect("tg_sqlite.db")
 cursor = db_connect.cursor()
 
-@client.on(events.NewMessage(pattern='\w{1,10} \w{1,10} https://t.me/joinchat/\w'))
+@client.on(events.NewMessage(pattern='\w{1,10} \w{1,10} https://t.me/\w{1,30}'))
 async def start(event):
     message = event.message.message
-    print(message)
     end_channel_id = event.message.peer_id.channel_id
-    print(end_channel_id)
     links = search("https://t.me/joinchat/\w{1,23}", message)
-    link = links.group(0)
-    group_hash = await clean_text(link)
-    print("Try to add chat " + group_hash)
-    try:
-        updates = await client(ImportChatInviteRequest(group_hash))
-        print(updates)
-        print(updates.chats[0])
-        chanel_title = updates.chats[0].title
-        chanel_id = updates.chats[0].id
-        message_text =  "Chat <a href='" + link + "'>" + chanel_title + "</a> add"
-        await event.respond(message_text)
-    except:
-        await event.respond("Chat alredy existed")
-    print(chanel_title,chanel_id, group_hash,end_channel_id)
+    if bool(links):
+        link = links.group(0)
+        group_hash = await clean_text_privat(link)
+        print("Try to add chat " + group_hash)
+        try:
+            updates = await client(ImportChatInviteRequest(group_hash))
+            print(updates.chats[0])
+            chanel_title = updates.chats[0].title
+            chanel_id = updates.chats[0].id
+            message_text =  "Chat <a href='" + link + "'>" + chanel_title + "</a> add"
+            await event.respond(message_text)
+        except:
+            await event.respond("Chat alredy existed")
     cursor.execute("insert into tg_channel (title,id,url,end_channel_id) values (?, ?, ?, ?)",(chanel_title,chanel_id, group_hash,end_channel_id))
     db_connect.commit()
     raise events.StopPropagation
@@ -53,7 +50,7 @@ async def handler(event):
     result_int = int(result[0])
     await client.send_message(result_int, event.message )
         
-async def clean_text(text):
+async def clean_text_privat(text):
     del_text = "https://t.me/joinchat/"
     new_text = sub(del_text, '', text)
     return new_text
