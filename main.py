1654736ff76bbf00ba4c87c92ff120c37fcff95f
Rhonstin <rhonstin@gmail.com>
2020-11-06 08:29:07 +0200

    add reply function

--- main.py
+++ main.py
@@ -12,34 +12,51 @@ api_hash = getenv("API_HASH")
 
 client = TelegramClient('Rhons', api_id, api_hash)
 client.parse_mode = "html"
-#sync def main():
-#   updates2 = await client(JoinChannelRequest('https://t.me/tg_bot_feader_test'))
 
 db_connect = sqlite3.connect("tg_sqlite.db")
 cursor = db_connect.cursor()
 
-@client.on(events.NewMessage(pattern='\w{1,10} \w{1,10} https://t.me/\w{1,30}'))
+@client.on(events.NewMessage(pattern='.{1,10} .{1,10} https://t.me/\w{1,30}'))
 async def start(event):
     message = event.message.message
-    end_channel_id = event.message.peer_id.channel_id
-    links = search("https://t.me/joinchat/\w{1,23}", message)
+    print(message)
+    try:
+        end_channel_id = event.message.peer_id.channel_id
+    except:
+        end_channel_id = event.message.peer_id.chat_id
+    links = search("https://t.me/joinchat/.{1,23}", message)
     if bool(links):
         link = links.group(0)
         group_hash = await clean_text_privat(link)
         print("Try to add chat " + group_hash)
         try:
             updates = await client(ImportChatInviteRequest(group_hash))
-            print(updates.chats[0])
-            chanel_title = updates.chats[0].title
-            chanel_id = updates.chats[0].id
-            message_text =  "Chat <a href='" + link + "'>" + chanel_title + "</a> add"
+            chanel_title,chanel_id,message_text = await generate_link(link,updates)
             await event.respond(message_text)
         except:
             await event.respond("Chat alredy existed")
-    cursor.execute("insert into tg_channel (title,id,url,end_channel_id) values (?, ?, ?, ?)",(chanel_title,chanel_id, group_hash,end_channel_id))
+    else:
+        links = search("https://t.me/.{1,40}", message)
+        link = links.group(0)
+        print("Try to add chat " + link)
+        try:
+            updates = await client(JoinChannelRequest(link))
+            chanel_title,chanel_id,message_text = await generate_link(link,updates)
+            await event.respond(message_text)
+        except:
+            await event.respond("Chat alredy existed")        
+    cursor.execute("insert into tg_channel (title,id,url,end_channel_id) values (?, ?, ?, ?)",(chanel_title,chanel_id,link,end_channel_id))
     db_connect.commit()
     raise events.StopPropagation
 
+
+async def generate_link(link,updates):
+    print(updates.chats[0])
+    chanel_title = updates.chats[0].title
+    chanel_id = updates.chats[0].id
+    message_text =  "Chat <a href='" + link + "'>" + chanel_title + "</a> add"
+    return chanel_title,chanel_id,message_text
+    
 @client.on(events.NewMessage())
 async def handler(event):
     channel_id = event.message.peer_id.channel_id
